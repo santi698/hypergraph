61210b89317c62c021adf77772ee3558f1b4e0c9
iojichervo <ioji@chervonagura.com.ar>
2013-05-25 19:52:28 -0300

    Ahora el nombre de los archivos de salida, depende del de entrada

--- src/view/ShortestPathCalculator.java
+++ src/view/ShortestPathCalculator.java
@@ -16,16 +16,22 @@ public class ShortestPathCalculator {
 	 */
 	public static void main(String[] args) {
 		TaskHypergraphParser parser = new TaskHypergraphParser();
-		String s;
+		String s, result;
 		try {
 			s = FileManager.getFileContents(args[0]);
+			result = args[0].substring(0, args[0].length() - 3);
 			TaskHypergraph graph = parser.parse(s);
+			
 			//TODO Crear una funcion que parsee la entrada y llame a la funcion con los argumentos correctos.
 			List<String> path = graph.getShortestPath(Approach.EXACT);
-			System.out.println("La longitud del camino m√°s corto es: " + path.size());
+			
+			System.out.println("La longitud del camino m·s corto es: " + path.size()); //ESTO NO ESTA BIEN, DEBERIA SER EL PESO 
+																					   //DE LA SUMA DEL CAMINO, NO LA CANTIDAD DE EJES
+			
 			TaskHypergraph subgraph = graph.getSubgraph(path);
-			FileManager.createFileWithContent("result.min.hg", subgraph.getHgRep());
-			FileManager.createFileWithContent("result.min.dot", subgraph.getDOTRep());
+			FileManager.createFileWithContent(result + ".dot", graph.getDOTRep());
+			FileManager.createFileWithContent(result + ".min.hg", subgraph.getHgRep());
+			FileManager.createFileWithContent(result + ".min.dot", graph.getDOTRep(path));
 		} catch (IOException e) {
 			System.err.println(e.getLocalizedMessage());
 		}
