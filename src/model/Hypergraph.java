1bffa937bcf5097adc8427a383f434db80581166
iojichervo <ioji@chervonagura.com.ar>
2013-05-25 19:54:37 -0300

    Ahora hay una lista de Hyperedge, se pueden agregar ejes con addHyperedge y los ejes requerian una variable String como nombre, esto esta aclarado en el formato de los .hg

--- src/model/Hypergraph.java
+++ src/model/Hypergraph.java
@@ -9,42 +9,74 @@ public abstract class Hypergraph<V> implements HypergraphIFace<V> {
 
 	private Map<V, Node> nodes;
 	private List<Node> nodeList;
-
+	private List<Hyperedge> edgeList;
+	
 	public Hypergraph() {
 		nodes = new HashMap<V, Node>();
 		nodeList = new LinkedList<Node>();
+		edgeList = new LinkedList<Hyperedge>();
 	}
 
 	@Override
-	public void addHyperedge(int weight, List<V> requisites,
-			List<V> consequences) {
-		// TODO Auto-generated method stub
+	public void addHyperedge(String name, int weight, List<V> requisites, List<V> consequences) {
+		Hyperedge edge = new Hyperedge(name, weight);
+		edgeList.add(edge);
+		
+		for(V data : requisites) {
+			Node node = addNode(data);
+			node.edges.add(edge);
+			edge.requisites.add(node);
+		}
+		
+		for(V data : consequences) {
+			Node node = addNode(data);
+			edge.consequences.add(node);
+		}
 	}
 
-	public void addNode(V data) {
-		if (!nodes.containsKey(data)) {
-			Node node = new Node(data);
+	protected Node addNode(V data) {
+		Node node = nodes.get(data);
+		if (node == null) {
+			node = new Node(data);
 			nodes.put(data, node);
 			nodeList.add(node);
+		}
+		return node;
+	}
+	
+	public void clearMarks() {
+		for(Node node : nodeList) {
+			node.visited = false;
+		}
+
+		for(Hyperedge edge : edgeList) {
+			edge.visited = false;
 		}
 	}
 
 	protected List<Node> getNodes() {
 		return nodeList;
 	}
+	
+	protected List<Hyperedge> getEdges() {
+		return edgeList;
+	}
 
 	protected class Hyperedge {
+		public String name;
+		public int weight;
 		public List<Node> requisites;
 		public List<Node> consequences;
-		public int weight;
 		public boolean visited;
 
-		public Hyperedge(int weight) {
+		public Hyperedge(String name, int weight) {
+			this.name = name;
 			this.weight = weight;
 			this.requisites = new LinkedList<Node>();
 			this.consequences = new LinkedList<Node>();
 			this.visited = false;
 		}
+		
 		public boolean isDone() {
 				for (Node n : requisites) {
 					if (!n.visited)
@@ -55,9 +87,9 @@ public abstract class Hypergraph<V> implements HypergraphIFace<V> {
 	}
 
 	protected class Node {
-		public boolean visited;
-		public List<Hyperedge> edges;
 		public V data;
+		public List<Hyperedge> edges;
+		public boolean visited;
 
 		public Node(V data) {
 			this.data = data;
